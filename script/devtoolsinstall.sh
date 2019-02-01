6f14ace83c42fb072f92e6a0d68d28267970eb6b
Michal Stawinski <extern.Michal.Stawinski@elektrobit.com>
2019-02-01 12:17:52 +0100

    Update dev scripts

--- script/devtoolsinstall.sh
+++ script/devtoolsinstall.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# essentials
 sudo apt-get install git
 sudo apt-get install build-essential
 sudo apt-get install ack
 sudo apt install python3-dev python3-pip python3-setuptools
 sudo pip3 install thefuck
 
-sudo apt-get install openjdk-8-jdk
-sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip
-sudo apt-get install git gitk wget locales
+# misc
 sudo apt-get install qemu-kvm cpu-checker
-sudo apt install gitk
+sudo apt-get install wget locales
+
+# android
+sudo apt-get install openjdk-8-jdk
+sudo apt-get install ccache
+sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip
+
+# private
+sudo apt-get install gitk
+sudo apt-get install byobu
+sudo apt-get install minicom
