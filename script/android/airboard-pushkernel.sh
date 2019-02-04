50579501933c053160533900498b5e052a706b56
Michal Stawinski <extern.Michal.Stawinski@elektrobit.com>
2019-02-04 17:32:25 +0100

    Switch from curlt to ftp-upload

--- script/android/airboard-pushkernel.sh
+++ script/android/airboard-pushkernel.sh
@@ -2,7 +2,7 @@
 
 AIRBOARD=192.168.1.1
 OVERLAY=$1
-./obj/kernel/msm-4.14/arch/arm64/boot/dts/qcom/sa8155-vm.dtb
-curl -T $OVERLAY/obj/kernel/msm-4.14/arch/arm64/boot/dts/qcom/sa8155-vm.dtb ftp://$AIRBOARD/vm/images --user root:
-curl -T $OVERLAY/boot.img ftp://$AIRBOARD/vm/images/linux.img --user root:
-curl -T $OVERLAY/ramdisk.img ftp://$AIRBOARD/vm/images/ramdisk.img --user root:
+
+ftp-upload --passive -h $AIRBOARD -u root -d /vm/images $OVERLAY/obj/kernel/msm-4.14/arch/arm64/boot/dts/qcom/sa8155-vm.dtb
+ftp-upload --passive -h $AIRBOARD -u root -d /vm/images --as linux.img $OVERLAY/boot.img
+ftp-upload --passive -h $AIRBOARD -u root -d /vm/images $OVERLAY/ramdisk.img
--- script/android/airboard-pushoverlay.sh
+++ script/android/airboard-pushoverlay.sh
@@ -3,6 +3,6 @@
 AIRBOARD=192.168.1.1
 OVERLAY=$1
 
-curl -T $OVERLAY/scripts/startup.sh ftp://$AIRBOARD/scripts --user root:
-curl -T $OVERLAY/scripts/android.sh ftp://$AIRBOARD/scripts --user root:
-curl -T $OVERLAY/bin/qcdisplaycfg.xml ftp://$AIRBOARD/bin --user root:
+ftp-upload --passive -h $AIRBOARD -u root -d /scripts $OVERLAY/scripts/startup.sh
+ftp-upload --passive -h $AIRBOARD -u root -d /scripts $OVERLAY/scripts/android.sh
+ftp-upload --passive -h $AIRBOARD -u root -d /bin     $OVERLAY/bin/qcdisplaycfg.xml
