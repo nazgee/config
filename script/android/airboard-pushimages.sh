e02946e66700e6a6ed38de875c0f3c68c5f09d98
Michal Stawinski <extern.Michal.Stawinski@elektrobit.com>
2019-02-05 12:28:01 +0100

    airboard: flash both vendor partitions

--- script/android/airboard-pushimages.sh
+++ script/android/airboard-pushimages.sh
@@ -2,6 +2,7 @@
 
 fastboot flash system_b system.img
 fastboot flash userdata userdata.img
+fastboot flash vendor_a vendor.img
 fastboot flash vendor_b vendor.img
 fastboot flash persist persist.img
 fastboot -w
