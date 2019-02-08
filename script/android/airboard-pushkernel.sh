c51d3d8591e56e7985a11a89d9227bd42467af38
Michal Stawinski <extern.Michal.Stawinski@elektrobit.com>
2019-02-08 18:08:36 +0100

    airboard: fix scripts

--- script/android/airboard-pushkernel.sh
+++ script/android/airboard-pushkernel.sh
@@ -4,5 +4,6 @@ AIRBOARD=192.168.1.1
 OVERLAY=$1
 
 ftp-upload --passive -h $AIRBOARD -u root -d /vm/images $OVERLAY/obj/kernel/msm-4.14/arch/arm64/boot/dts/qcom/sa8155-vm.dtb
+ftp-upload --passive -h $AIRBOARD -u root -d /vm/images $OVERLAY/obj/kernel/msm-4.14/arch/arm64/boot/dts/qcom/sa8155p.dtb
 ftp-upload --passive -h $AIRBOARD -u root -d /vm/images --as linux.img $OVERLAY/obj/KERNEL_OBJ/arch/arm64/boot/Image
 ftp-upload --passive -h $AIRBOARD -u root -d /vm/images $OVERLAY/ramdisk.img
--- script/android/airboard-pushoverlay.sh
+++ script/android/airboard-pushoverlay.sh
@@ -6,3 +6,5 @@ OVERLAY=$1
 ftp-upload --passive -h $AIRBOARD -u root -d /scripts $OVERLAY/scripts/startup.sh
 ftp-upload --passive -h $AIRBOARD -u root -d /scripts $OVERLAY/scripts/android.sh
 ftp-upload --passive -h $AIRBOARD -u root -d /bin     $OVERLAY/bin/qcdisplaycfg.xml
+ftp-upload --passive -h $AIRBOARD -u root -d /etc/system/config $OVERLAY/etc/system/config/mtouch_lilliput_1080p_eGalax_FIRST.conf
+ftp-upload --passive -h $AIRBOARD -u root -d /etc/system/config $OVERLAY/etc/system/config/mtouch_lilliput_1080p_eGalax_FIRST
