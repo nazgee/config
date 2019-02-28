289833aed787521f9c73248d1d63936816cff738
Michal Stawinski <extern.Michal.Stawinski@elektrobit.com>
2019-02-28 12:57:04 +0100

    airboard: fix vendor partition name

--- script/android/airboard-pushimages.sh
+++ script/android/airboard-pushimages.sh
@@ -5,6 +5,6 @@ _out=$1
 fastboot flash system_b $_out/system.img
 fastboot flash userdata $_out/userdata.img
 fastboot flash vendor_a $_out/vendor.img
-fastboot flash vendor_b $_out/tvendor.img
+fastboot flash vendor_b $_out/vendor.img
 fastboot flash persist $_out/persist.img
 fastboot -w
