234410b13826db56c5192dd8ae4a79520870caab
Michal Stawinski <michal.stawinski@gmail.com>
2023-01-21 04:26:53 +0100

    update devtools

--- script/util/devtoolsinstall.sh
+++ script/util/devtoolsinstall.sh
@@ -14,10 +14,8 @@ sudo apt-get install qemu-kvm cpu-checker
 sudo apt-get install wget locales
 
 # android
-sudo apt-get install openjdk-8-jdk
-sudo apt-get install ccache
-sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip
-sudo apt-get install libssl-dev
+sudo apt-get install repo
+sudo apt-get install git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
 
 # private
 sudo apt-get install gitk
