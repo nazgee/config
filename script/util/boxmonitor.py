c637d29ca5facb8cd5377453e3fc0d14a03d59ce
Michal Stawinski <michal.stawinski@gmail.com>
2023-01-26 10:37:53 +0100

    boxmonitor: Add time elapsed

--- script/util/boxmonitor.py
+++ script/util/boxmonitor.py
@@ -3,54 +3,87 @@
 import subprocess
 import argparse
 import signal
+import time
 
-def on_success(timeout, msg):
-    print("OK: " + msg)
+
+# Class that measures the time between two events
+class Timer:
+    def __init__(self):
+        self.start = time.time()
+
+    def elapsed(self):
+        return time.time() - self.start
+
+    def reset(self):
+        self.start = time.time()
+
+    # returns the string representing elapsed elapsed
+    # time in as e.g. 1h:2m:33s or 4m:12s or 12s
+    def elapsed_str(self):
+        elapsed = self.elapsed()
+        hours = int(elapsed / 3600)
+        minutes = int((elapsed - hours * 3600) / 60)
+        seconds = int(elapsed - hours * 3600 - minutes * 60)
+
+        if hours > 0:
+            return f"Time used: {hours}h:{minutes}m:{seconds}s"
+        elif minutes > 0:
+            return f"Time used: {minutes}m:{seconds}s"
+        else:
+            return f"Time used: {seconds}s"
+
+
+elapsedTimer: Timer = None
+completed: subprocess.CompletedProcess = None
+
+
+def on_success(msg):
+    print(msg + "\n" + elapsedTimer.elapsed_str())
     subprocess.run(["openrgb", "-p", "ok"], stdout=subprocess.DEVNULL)
 
 
-def on_failure(timeout, msg):
-    print("FAIL: " + msg)
+def on_failure(msg):
+    print(msg + "\n" + elapsedTimer.elapsed_str())
     subprocess.run(["openrgb", "-p", "error"], stdout=subprocess.DEVNULL)
 
 
-def on_idle(timeout, msg):
-    print("IDLE: " + msg)
+def on_idle(msg):
+    print(msg + "\n" + elapsedTimer.elapsed_str())
     subprocess.run(["openrgb", "-p", "idle"], stdout=subprocess.DEVNULL)
 
 
-completed : subprocess.CompletedProcess = None
-
-
 def handler(signum, frame):
-    on_idle(0, "interrupted")
+    on_idle("INTERRUPTED!")
     exit(1)
 
 
-def run_subprocess(args, timeout):
-    signal.signal(signal.SIGINT, handler)
+def run_subprocess(args):
+    global elapsedTimer
+    global completed
 
+    signal.signal(signal.SIGINT, handler)
     try:
         subprocess.run(["openrgb", "-p", "busy"], stdout=subprocess.DEVNULL)
+        elapsedTimer = Timer()
         completed = subprocess.run(args)
         if completed.returncode < 0:
-            on_failure(timeout, f"terminated by signal {-completed.returncode}")
+            on_failure(f"WARNNG! Terminated by signal {-completed.returncode}")
         elif completed.returncode == 0:
-            on_success(timeout, f"ok")
+            on_success(f"OK")
         else:
-            on_failure(timeout, f"failed with {completed.returncode}")
+            on_failure(f"ERROR! Failed with {completed.returncode}")
     except OSError as e:
-        on_failure(timeout, f"failed with {e}")
+        on_failure(f"ERROR! Failed with {e}")
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("command", nargs=argparse.REMAINDER, help="Command to run and its arguments")
-    parser.add_argument("--timeout", type=int, help="Timeout for the command in seconds")
     args = parser.parse_args()
 
     if not args.command:
         on_idle(0, "startup")
         parser.error("Command is required")
 
-    run_subprocess(args.command, timeout=args.timeout)
+    run_subprocess(args.command)
